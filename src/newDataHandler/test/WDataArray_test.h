43b92b27783273458c7ffadede176d2a00be336c
Sebastian Eichelbaum <eichelbaum@informatik.uni-leipzig.de>
2010-07-26 16:34:56 +0200

    [ADD] - added dummy test to test tests

--- src/newDataHandler/test/WDataArray_test.h
+++ src/newDataHandler/test/WDataArray_test.h
@@ -1,1 +1,46 @@
+//---------------------------------------------------------------------------
+//
+// Project: OpenWalnut ( http://www.openwalnut.org )
+//
+// Copyright 2009 OpenWalnut Community, BSV@Uni-Leipzig and CNCF@MPI-CBS
+// For more information see http://www.openwalnut.org/copying
+//
+// This file is part of OpenWalnut.
+//
+// OpenWalnut is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// OpenWalnut is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with OpenWalnut. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------
+
+#ifndef WDATAARRAY_TEST_H
+#define WDATAARRAY_TEST_H
+
+#include <cxxtest/TestSuite.h>
+
+#include "../WDataArray.h"
+
+/**
+ * Tests the WDataArray class.
+ */
+class WDataArrayTest : public CxxTest::TestSuite
+{
+public:
+
+    void testInstantiation( void )
+    {
+        TS_ASSERT( true );
+    }
+};
+
+#endif  // WDATAARRAY_TEST_H
 
