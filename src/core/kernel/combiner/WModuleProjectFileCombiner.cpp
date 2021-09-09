2d9243ed8a7d494229710dc22b39296a5ba02bf5
Robin Eschbach <robin.eschbach@hs-worms.de>
2021-09-09 13:27:45 +0200

    [FIX #540] ignore connections to inner modules of containers

--- src/core/kernel/combiner/WModuleProjectFileCombiner.cpp
+++ src/core/kernel/combiner/WModuleProjectFileCombiner.cpp
@@ -446,8 +446,11 @@ void WModuleProjectFileCombiner::save( std::ostream& output )   // NOLINT
             {
                 // as the module is a weak_ptr -> lock and get access to it
                 std::shared_ptr< WModule > theOtherModule = ( *iciter )->m_module.lock();
-                output << "CONNECTION:(" << moduleToIDMap[ ( *iter ) ] << "," << ( *citer )->getName() << ")->(" <<
-                                            moduleToIDMap[ theOtherModule ] << "," << ( *iciter )->getName() << ")" << std::endl;
+                if( moduleToIDMap.find( theOtherModule ) != moduleToIDMap.end() )
+                {
+                    output << "CONNECTION:(" << moduleToIDMap[ ( *iter ) ] << "," << ( *citer )->getName() << ")->(" <<
+                                                moduleToIDMap[ theOtherModule ] << "," << ( *iciter )->getName() << ")" << std::endl;
+                }
             }
             lock.unlock();
         }
