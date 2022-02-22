451fd9097720eb4bd484d0a1a423ba6b1c2397b3
Nico Henkel <nico.henkel1@web.de>
2022-02-23 00:07:01 +0100

    [STYLE] fix accidental auto format style issues

--- src/modules/openVRCamera/WMVRCamera.cpp
+++ src/modules/openVRCamera/WMVRCamera.cpp
@@ -28,7 +28,7 @@
 #include "WMVRCamera.xpm"
 
 // This line is needed by the module loader to actually find your module.
-W_LOADABLE_MODULE(WMVRCamera)
+W_LOADABLE_MODULE( WMVRCamera )
 
 WMVRCamera::WMVRCamera() : WModule()
 {
@@ -40,7 +40,7 @@ WMVRCamera::~WMVRCamera()
 
 std::shared_ptr<WModule> WMVRCamera::factory() const
 {
-    return std::shared_ptr<WModule>(new WMVRCamera());
+    return std::shared_ptr<WModule>( new WMVRCamera() );
 }
 
 const char **WMVRCamera::getXPMIcon() const
@@ -66,12 +66,12 @@ void WMVRCamera::connectors()
 
 void WMVRCamera::properties()
 {
-    m_propCondition = std::shared_ptr<WCondition>(new WCondition());
+    m_propCondition = std::shared_ptr<WCondition>( new WCondition() );
 
-    m_vrOn = m_properties->addProperty("Submit Frames", "Toggle submitting frames to OpenVR", false);
-    m_VR_fpsTrigger = m_properties->addProperty("Log Fps", "Now", WPVBaseTypes::PV_TRIGGER_READY);
-    m_VR_logCameraViewMatrix = m_properties->addProperty("Log Camera View Matrix", "Now", WPVBaseTypes::PV_TRIGGER_READY);
-    m_VR_resetHMDPosition = m_properties->addProperty("Reset position", "Reset", WPVBaseTypes::PV_TRIGGER_READY);
+    m_vrOn = m_properties->addProperty( "Submit Frames", "Toggle submitting frames to OpenVR", false );
+    m_VR_fpsTrigger = m_properties->addProperty( "Log Fps", "Now", WPVBaseTypes::PV_TRIGGER_READY );
+    m_VR_logCameraViewMatrix = m_properties->addProperty( "Log Camera View Matrix", "Now", WPVBaseTypes::PV_TRIGGER_READY );
+    m_VR_resetHMDPosition = m_properties->addProperty( "Reset position", "Reset", WPVBaseTypes::PV_TRIGGER_READY );
 
     WModule::properties();
 }
@@ -79,7 +79,7 @@ void WMVRCamera::properties()
 bool WMVRCamera::setupVRInterface()
 {
     // Exit if we do not have an HMD present
-    if (!vr::VR_IsHmdPresent())
+    if ( !vr::VR_IsHmdPresent() )
     {
         errorLog() << "No valid HMD present!" << std::endl;
         return false;
@@ -87,18 +87,18 @@ bool WMVRCamera::setupVRInterface()
 
     // Loading the OpenVR Runtime
     vr::EVRInitError eError = vr::VRInitError_None;
-    m_vrSystem = vr::VR_Init(&eError, vr::VRApplication_Scene);
+    m_vrSystem = vr::VR_Init( &eError, vr::VRApplication_Scene );
 
-    if (eError != vr::VRInitError_None)
+    if ( eError != vr::VRInitError_None )
     {
         m_vrSystem = nullptr;
         errorLog()
             << "Unable to initialize the OpenVR library." << std::endl
-            << "Reason: " << vr::VR_GetVRInitErrorAsEnglishDescription(eError) << std::endl;
+            << "Reason: " << vr::VR_GetVRInitErrorAsEnglishDescription( eError ) << std::endl;
         return false;
     }
 
-    if (!vr::VRCompositor())
+    if ( !vr::VRCompositor() )
     {
         m_vrSystem = nullptr;
         vr::VR_Shutdown();
@@ -106,26 +106,26 @@ bool WMVRCamera::setupVRInterface()
         return false;
     }
 
-    m_vrRenderModels = (vr::IVRRenderModels *)vr::VR_GetGenericInterface(vr::IVRRenderModels_Version, &eError);
+    m_vrRenderModels = ( vr::IVRRenderModels *)vr::VR_GetGenericInterface(vr::IVRRenderModels_Version, &eError );
 
-    if (m_vrRenderModels == nullptr)
+    if ( m_vrRenderModels == nullptr )
     {
         m_vrSystem = nullptr;
         vr::VR_Shutdown();
         errorLog()
             << "Unable to get render model interface!" << std::endl
-            << "Reason: " << vr::VR_GetVRInitErrorAsEnglishDescription(eError) << std::endl;
+            << "Reason: " << vr::VR_GetVRInitErrorAsEnglishDescription( eError ) << std::endl;
         return false;
     }
 
-    vr::VRCompositor()->SetTrackingSpace(vr::TrackingUniverseSeated);
-    m_vrSystem->GetRecommendedRenderTargetSize(&m_vrRenderWidth, &m_vrRenderHeight);
+    vr::VRCompositor()->SetTrackingSpace( vr::TrackingUniverseSeated );
+    m_vrSystem->GetRecommendedRenderTargetSize( &m_vrRenderWidth, &m_vrRenderHeight );
 
     m_grabber = vr::k_unTrackedDeviceIndexInvalid;
     updateDeviceIDs();
 
-    std::string driverName = getDeviceProperty(vr::Prop_TrackingSystemName_String);
-    std::string deviceSerialNumber = getDeviceProperty(vr::Prop_SerialNumber_String);
+    std::string driverName = getDeviceProperty( vr::Prop_TrackingSystemName_String );
+    std::string deviceSerialNumber = getDeviceProperty( vr::Prop_SerialNumber_String );
     debugLog() << "HMD driver name: " << driverName;
     debugLog() << "HMD device serial number: " << deviceSerialNumber;
 
@@ -134,14 +134,14 @@ bool WMVRCamera::setupVRInterface()
 
 void WMVRCamera::moduleMain()
 {
-    m_moduleState.setResetable(true, true);
-    m_moduleState.add(m_propCondition);
+    m_moduleState.setResetable( true, true );
+    m_moduleState.add( m_propCondition );
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
     // HMD Setup
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    if (setupVRInterface())
+    if ( setupVRInterface() )
     {
         debugLog() << "OpenVR initialized successfully";
         m_vrIsInitialized = true;
@@ -158,31 +158,31 @@ void WMVRCamera::moduleMain()
     // Render to Texture Setup
     /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    osg::View *mainView = WKernel::getRunningKernel()->getGraphicsEngine()->getViewer()->getView();
-    osg::GraphicsContext *gc = mainView->getCamera()->getGraphicsContext();
-    osg::ref_ptr<WGEGroupNode> scene = WKernel::getRunningKernel()->getGraphicsEngine()->getScene();
+    osg::View* mainView = WKernel::getRunningKernel()->getGraphicsEngine()->getViewer()->getView();
+    osg::GraphicsContext* gc = mainView->getCamera()->getGraphicsContext();
+    osg::ref_ptr< WGEGroupNode > scene = WKernel::getRunningKernel()->getGraphicsEngine()->getScene();
 
-    osg::Matrix projLeft = convertHmdMatrixToOSG(m_vrSystem->GetProjectionMatrix(vr::Eye_Left, 1.0, 1000.0));
-    osg::Matrix projRight = convertHmdMatrixToOSG(m_vrSystem->GetProjectionMatrix(vr::Eye_Right, 1.0, 1000.0));
+    osg::Matrix projLeft = convertHmdMatrixToOSG( m_vrSystem->GetProjectionMatrix( vr::Eye_Left, 1.0, 1000.0 ) );
+    osg::Matrix projRight = convertHmdMatrixToOSG( m_vrSystem->GetProjectionMatrix( vr::Eye_Right, 1.0, 1000.0 ) );
 
-    m_leftEyeCamera = new WRTTCamera(m_vrRenderWidth, m_vrRenderHeight, gc, projLeft, scene, m_localPath);
-    m_rightEyeCamera = new WRTTCamera(m_vrRenderWidth, m_vrRenderHeight, gc, projRight, scene, m_localPath);
+    m_leftEyeCamera = new WRTTCamera( m_vrRenderWidth, m_vrRenderHeight, gc, projLeft, scene, m_localPath );
+    m_rightEyeCamera = new WRTTCamera( m_vrRenderWidth, m_vrRenderHeight, gc, projRight, scene, m_localPath );
 
-    mainView->addSlave(m_leftEyeCamera, false);
-    mainView->addSlave(m_rightEyeCamera, false);
+    mainView->addSlave( m_leftEyeCamera, false );
+    mainView->addSlave( m_rightEyeCamera, false );
 
     // updatecallback for submitting frames
-    gc->setSwapCallback(new WSwapUpdateCallback(this));
+    gc->setSwapCallback( new WSwapUpdateCallback(this) );
 
     // Now, we can mark the module ready.
     ready();
     debugLog() << "Starting...";
 
     // loop until the module container requests the module to quit
-    while (!m_shutdownFlag())
+    while ( !m_shutdownFlag() )
     {
         // woke up since the module is requested to finish
-        if (m_shutdownFlag())
+        if ( m_shutdownFlag() )
         {
             break;
         }
@@ -192,7 +192,7 @@ void WMVRCamera::moduleMain()
     }
 
     // remove update callback
-    gc->setSwapCallback(nullptr);
+    gc->setSwapCallback( nullptr );
 
     // Shut down OpenVR
     m_vrSystem = nullptr;
@@ -203,7 +203,7 @@ void WMVRCamera::moduleMain()
 
 void WMVRCamera::activate()
 {
-    if (m_active->get())
+    if ( m_active->get() )
     {
         debugLog() << "Activate.";
     }
@@ -215,44 +215,44 @@ void WMVRCamera::activate()
     WModule::activate();
 }
 
-std::string WMVRCamera::getDeviceProperty(vr::TrackedDeviceProperty prop)
+std::string WMVRCamera::getDeviceProperty( vr::TrackedDeviceProperty prop )
 {
-    uint32_t bufferLen = m_vrSystem->GetStringTrackedDeviceProperty(vr::k_unTrackedDeviceIndex_Hmd, prop, NULL, 0);
-    if (bufferLen == 0)
+    uint32_t bufferLen = m_vrSystem->GetStringTrackedDeviceProperty( vr::k_unTrackedDeviceIndex_Hmd, prop, NULL, 0 );
+    if( bufferLen == 0 )
     {
         return "";
     }
 
     char *buffer = new char[bufferLen];
-    bufferLen = m_vrSystem->GetStringTrackedDeviceProperty(vr::k_unTrackedDeviceIndex_Hmd, prop, buffer, bufferLen);
+    bufferLen = m_vrSystem->GetStringTrackedDeviceProperty( vr::k_unTrackedDeviceIndex_Hmd, prop, buffer, bufferLen );
     std::string result = buffer;
     delete[] buffer;
     return result;
 }
 
-void WMVRCamera::handleVREvent(vr::VREvent_t vrEvent)
+void WMVRCamera::handleVREvent( vr::VREvent_t vrEvent )
 {
-    vr::ETrackedDeviceClass deviceClass = m_vrSystem->GetTrackedDeviceClass(vrEvent.trackedDeviceIndex);
-    switch (deviceClass)
+    vr::ETrackedDeviceClass deviceClass = m_vrSystem->GetTrackedDeviceClass( vrEvent.trackedDeviceIndex );
+    switch( deviceClass )
     {
     case vr::TrackedDeviceClass_Controller:
-        handleControllerEvent(vrEvent);
+        handleControllerEvent( vrEvent );
         break;
     default:
         break;
     }
 }
 
-void WMVRCamera::handleControllerEvent(vr::VREvent_t vrEvent)
+void WMVRCamera::handleControllerEvent( vr::VREvent_t vrEvent )
 {
-    switch (vrEvent.eventType)
+    switch( vrEvent.eventType )
     {
     case vr::VREvent_ButtonPress:
         // check for any changes regarding controller device IDs on any button press
         updateDeviceIDs();
 
         debugLog() << "Pressed:" << vrEvent.data.controller.button;
-        if (
+        if(
             vrEvent.data.controller.button == vr::EVRButtonId::k_EButton_Grip ||
             vrEvent.data.controller.button == vr::EVRButtonId::k_EButton_SteamVR_Trigger)
         {
@@ -261,7 +261,7 @@ void WMVRCamera::handleControllerEvent(vr::VREvent_t vrEvent)
         break;
     case vr::VREvent_ButtonUnpress:
         debugLog() << "Unpressed:" << vrEvent.data.controller.button;
-        if (
+        if(
             vrEvent.data.controller.button == vr::EVRButtonId::k_EButton_Grip ||
             vrEvent.data.controller.button == vr::EVRButtonId::k_EButton_SteamVR_Trigger)
         {
@@ -275,12 +275,12 @@ void WMVRCamera::handleControllerEvent(vr::VREvent_t vrEvent)
 
 void WMVRCamera::updateDeviceIDs()
 {
-    for ( uint32_t i = 0; i < vr::k_unMaxTrackedDeviceCount; i++ )
+    for( uint32_t i = 0; i < vr::k_unMaxTrackedDeviceCount; i++ )
     {
         vr::ETrackedControllerRole cRole = m_vrSystem->GetControllerRoleForTrackedDeviceIndex( i );
 
-        if ( cRole == vr::TrackedControllerRole_LeftHand ) m_controllerLeft_deviceID = i;
-        else if ( cRole == vr::TrackedControllerRole_RightHand ) m_controllerRight_deviceID = i;
+        if( cRole == vr::TrackedControllerRole_LeftHand ) m_controllerLeft_deviceID = i;
+        else if( cRole == vr::TrackedControllerRole_RightHand ) m_controllerRight_deviceID = i;
         // more devices IDs can be tracked here (such as the sonsors)        
     }
 }
@@ -291,7 +291,7 @@ void WMVRCamera::updateControllerPoses()
 	vr::VRControllerState_t controllerState;
 
     // Left Hand
-    if ( m_vrSystem->GetControllerRoleForTrackedDeviceIndex( m_controllerLeft_deviceID ) == vr::TrackedControllerRole_LeftHand )
+    if( m_vrSystem->GetControllerRoleForTrackedDeviceIndex( m_controllerLeft_deviceID ) == vr::TrackedControllerRole_LeftHand )
     {
         m_vrSystem->GetControllerStateWithPose( vr::TrackingUniverseStanding,
         m_controllerLeft_deviceID, &controllerState, sizeof( controllerState ),
@@ -300,7 +300,7 @@ void WMVRCamera::updateControllerPoses()
         m_controllerLeft_transform = convertHmdMatrixToOSG( trackedDevicePose.mDeviceToAbsoluteTracking );
     }
     // Right Hand
-    if ( m_vrSystem->GetControllerRoleForTrackedDeviceIndex( m_controllerRight_deviceID ) == vr::TrackedControllerRole_RightHand )
+    if( m_vrSystem->GetControllerRoleForTrackedDeviceIndex( m_controllerRight_deviceID ) == vr::TrackedControllerRole_RightHand )
     {
         m_vrSystem->GetControllerStateWithPose( vr::TrackingUniverseStanding,
         m_controllerRight_deviceID, &controllerState, sizeof( controllerState ),
@@ -316,26 +316,26 @@ void WMVRCamera::updateHMDPose()
     updateControllerPoses();
 
     // get all OpenVR tracking information
-    for (uint32_t i = 0; i < vr::k_unMaxTrackedDeviceCount; ++i)
+    for( uint32_t i = 0; i < vr::k_unMaxTrackedDeviceCount; ++i )
     {
         m_poses[i].bPoseIsValid = false;
     }
-    vr::VRCompositor()->WaitGetPoses(m_poses, vr::k_unMaxTrackedDeviceCount, NULL, 0);
+    vr::VRCompositor()->WaitGetPoses( m_poses, vr::k_unMaxTrackedDeviceCount, NULL, 0 );
 
     // handle controller events
     vr::VREvent_t vrEvent;
-    while (m_vrSystem->PollNextEvent(&vrEvent, sizeof(vr::VREvent_t)))
-        handleVREvent(vrEvent);
+    while( m_vrSystem->PollNextEvent( &vrEvent, sizeof( vr::VREvent_t ) ) )
+        handleVREvent( vrEvent );
 
     vr::TrackedDevicePose_t trackedDevicePose = m_poses[vr::k_unTrackedDeviceIndex_Hmd];
 
-    if (!trackedDevicePose.bPoseIsValid)
+    if( !trackedDevicePose.bPoseIsValid )
     {
         return;
     }
 
     // pose calculation
-    osg::Matrix poseTransform = convertHmdMatrixToOSG(trackedDevicePose.mDeviceToAbsoluteTracking);
+    osg::Matrix poseTransform = convertHmdMatrixToOSG( trackedDevicePose.mDeviceToAbsoluteTracking );
 
     osg::Vec3 position = poseTransform.getTrans() * 100.0;
     m_HMD_rotation = poseTransform.getRotate().inverse();
@@ -350,25 +350,25 @@ void WMVRCamera::updateHMDPose()
 
 void WMVRCamera::ResetHMDPosition()
 {
-    vr::VRChaperone()->ResetZeroPose(vr::TrackingUniverseSeated);
+    vr::VRChaperone()->ResetZeroPose( vr::TrackingUniverseSeated );
 }
 
-osg::Matrix WMVRCamera::convertHmdMatrixToOSG(const vr::HmdMatrix34_t &mat34)
+osg::Matrix WMVRCamera::convertHmdMatrixToOSG( const vr::HmdMatrix34_t &mat34 )
 {
     osg::Matrix matrix(
         mat34.m[0][0], mat34.m[1][0], mat34.m[2][0], 0.0,
         mat34.m[0][1], mat34.m[1][1], mat34.m[2][1], 0.0,
         mat34.m[0][2], mat34.m[1][2], mat34.m[2][2], 0.0,
-        mat34.m[0][3], mat34.m[1][3], mat34.m[2][3], 1.0f);
+        mat34.m[0][3], mat34.m[1][3], mat34.m[2][3], 1.0f );
     return matrix;
 }
 
-osg::Matrix WMVRCamera::convertHmdMatrixToOSG(const vr::HmdMatrix44_t &mat44)
+osg::Matrix WMVRCamera::convertHmdMatrixToOSG( const vr::HmdMatrix44_t &mat44 )
 {
     osg::Matrix matrix(
         mat44.m[0][0], mat44.m[1][0], mat44.m[2][0], mat44.m[3][0],
         mat44.m[0][1], mat44.m[1][1], mat44.m[2][1], mat44.m[3][1],
         mat44.m[0][2], mat44.m[1][2], mat44.m[2][2], mat44.m[3][2],
-        mat44.m[0][3], mat44.m[1][3], mat44.m[2][3], mat44.m[3][3]);
+        mat44.m[0][3], mat44.m[1][3], mat44.m[2][3], mat44.m[3][3] );
     return matrix;
 }
